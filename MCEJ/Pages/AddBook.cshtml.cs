3a408f64b790872f5ecdc2da9a11587fd9f39d48
cajsanilsson <143598251+cajsanilsson@users.noreply.github.com>
2024-05-02 13:10:20 +0200

    commit

---
+++ MCEJ/Pages/AddBook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MCEJ.Pages
+{
+    public class AddBookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
