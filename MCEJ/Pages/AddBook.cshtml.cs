9e4598985b479ba0348ebc96c7514dcbf6d5c35d
cajsanilsson <143598251+cajsanilsson@users.noreply.github.com>
2024-05-02 13:45:20 +0200

    commit

--- MCEJ/Pages/AddBook.cshtml.cs
+++ MCEJ/Pages/AddBook.cshtml.cs
@@ -1,3 +1,5 @@
+using MCEJ.Objects;
+using MCEJ.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,49 @@ namespace MCEJ.Pages
 {
     public class AddBookModel : PageModel
     {
+        readonly ILogger<AddBookModel> _logger;
+
+        public Book book; 
+
+        readonly DatabaseContext _databaseContext;
+
+        BookService _bookService;
+
+        List<Book> books;
+
+
+
+        
+
+        [BindProperty]
+        public string Message { get; set; }
+
+        public AddBookModel(ILogger<AddBookModel> logger, DatabaseContext databaseContext, BookService bookService)
+        {
+            _bookService = bookService;
+            _logger = logger;
+            _databaseContext = databaseContext;
+
+        }
+
         public void OnGet()
         {
         }
+
+        public void OnPost(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                _databaseContext.Books.Add(book);
+                _databaseContext.SaveChanges();
+
+            }
+
+            books = _databaseContext.Books.ToList();
+
+        
+        }
     }
+  
+    
 }
