62e8c960df9c3089b9eb3ff44d6f2c3f2cbc4647
MalinJonasson <147442348+MalinJonasson@users.noreply.github.com>
2024-05-08 08:52:41 +0200

    added empty constructor to book

--- Library Client/Book.cs
+++ Library Client/Book.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Library_Client
@@ -22,6 +23,7 @@ namespace Library_Client
 
 
         //Constructors
+        public Book() { }
         public Book(int bookId, string title, string author, string description, string pages)
         {
             BookId = bookId;
